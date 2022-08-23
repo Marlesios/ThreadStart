407e76a12d2b978252c42df3de1c83a97420d105
Marlesios <marles.nuralyev@gmail.com>
2022-08-23 17:56:16 +0300

    names of threads changed

--- src/main/java/com/example/Thread/Main.java
+++ src/main/java/com/example/Thread/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         ThreadGroup mainGroup = new ThreadGroup("mainGroup");
-        Thread thread1 = new Thread(mainGroup,new MyThread(),"hi i am the first");
-        Thread thread2 = new Thread(mainGroup, new MyThread(),"hi i am the second");
-        Thread thread3 = new Thread(mainGroup, new MyThread(),"hello i'm third");
-        Thread thread4 = new Thread(mainGroup, new MyThread(),"whats up i am number 4");
+        Thread thread1 = new Thread(mainGroup,new MyThread(),"номер 1");
+        Thread thread2 = new Thread(mainGroup, new MyThread(),"тот самый второй");
+        Thread thread3 = new Thread(mainGroup, new MyThread(),"под номером 3");
+        Thread thread4 = new Thread(mainGroup, new MyThread(),"под номером 4");
 
         thread1.start();
         thread2.start();
